0d07803151348ef8c6d7f922aa49ba7f8a53939c
mamtawardhani <53176352+mamtawardhani@users.noreply.github.com>
2021-03-31 19:40:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+var Settings = {
+  container: document.getElementById("guitar-tuner"),
+  backgroundColor: "white", // or hex colors "#ffffff"
+  notOkayColor: "orange",
+  okayColor: "green",
+  fontColor: "black"
+};
+
+function initializeTuner() {
+  // Create a single or multiple instance of tuners at time
+  var tuners = [
+    new OnlineTuner.Controller.GuitareTuner(
+      new OnlineTuner.Widget.CircleWidget(
+        Settings.container, 
+        Settings.backgroundColor, 
+        Settings.notOkayColor, 
+        Settings.okayColor, 
+        Settings.fontColor
+      )
+    )
+  ];
+
+  // Initialize the tuner with the callbacks
+  new OnlineTuner.Analyser(tuners).install(function() {
+    console.log("Succesfully initialized");
+
+  }, function(errorMessage) {
+    console.error("Oops, this shouldn't happen", errorMessage);
+  });
+}
+  
+    // Render the guitar tuner on the canvas by running the function
+initializeTuner();
